1c5f3ed78ea9baca04c3e894dec83deae15c2ceb
info <info@info26.cshl.edu>
2019-10-16 16:36:02 -0400

    updated 7.py

--- Python2/7.py
+++ Python2/7.py
@@ -4,14 +4,14 @@ import sys
 
 value = int( sys.argv[1]) #assigns value to integer of sys.argv[1] input
 
+print('value tested:', value) #prints the number being tested
+
 if value > 0:
 	print('positive') #print positive if input is positive
 	if value <50: #adds nested test to see if value is less than 50 
 		print ('smaller than 50') 
 		if value % 2 == 0: #adds nested test to see if value is even 
-			print ('value is even')
-		if value <50 and value % 2 ==0: 
-			print('it is an even number that is smaller than 50')
+			print ('value is even and smaller than 50')
 	else: 
 		print('larger than 50') #prints that value is larger than 50
 		if value % 3 ==0: #nested test- is number divisible by 3?
