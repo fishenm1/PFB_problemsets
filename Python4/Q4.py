993591e2d8dca451c4b6e198cb834fec4d87c1ac
info <info@info26.cshl.edu>
2019-10-17 20:57:09 -0400

    add Q4.py to Python4

---
+++ Python4/Q4.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+#write a script that uses a while loop to print out the numbers 1 to 100
+
+count =1
+while count <= 100:
+	print ('count:' , count)
+	count +=1
+print ('done')
+
+
