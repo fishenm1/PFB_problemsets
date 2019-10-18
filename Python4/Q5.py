44856af2c72bc01afcba1187e92cf21c55f69e9a
info <info@info26.cshl.edu>
2019-10-17 21:40:16 -0400

    added Q5.py

---
+++ Python4/Q5.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+#write a script that uses a while loop to calculate the factorial of 1000
+
+n = 5
+k = 1
+while n>0:
+	# print (n*k)
+	k  = n*k
+	n -= 1
+	print(k)
+print ('done') 
