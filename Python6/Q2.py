6d2301a8d9c15375dbd79ce0650a8bc793e6c5c0
info <info@info26.cshl.edu>
2019-10-18 11:34:08 -0400

    update Q2.py

--- Python6/Q2.py
+++ Python6/Q2.py
@@ -1,8 +1,8 @@
-#!/usr/bin/env python3 
+i#!/usr/bin/env python3 
 
 #open and read file python06.txt, uppercase each line, and print out eachto the STDOUT
 
-with open ("python06.txt", "r") as file, open ("Python_06.seq.txt", "w") as output_file:
+with open ("python06.txt", "r") as file, open ("Python_06.uc.txt", "w") as output_file:
 	for line in file: 
 		line_upper = line.upper()
 		line_strip = line_upper.rstrip()
