da06fbf1a339208b0be449cd7d5e236b0d6e7c30
info <info@info26.cshl.edu>
2019-10-24 16:20:46 -0400

    add seq2kmer.py

---
+++ RNAseq/seq2kmer.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+import os, sys 
+
+## method: sequence_to_kmer_list(sequence, kmer_length)
+##
+##  Extracts all kmers of a specified length from a sequence
+##
+##  ie.  sequence: GATCGATCGATCGA
+##   and given kmer_length = 4
+##   would yield
+##                 GATC
+##                  ATCG
+##                   TCGA
+##                    .... and so forth
+##                       
+##  input parameters:
+##
+##  seuqence : nucleotide sequence (type: string)
+##
+##  returns kmer_list : list of kmer sequences.
+##                    ie.  ["GATC", "ATCG", ...]
+
+
+def sequence_to_kmer_list(sequence, kmer_length): 
+	kmers_list = []
+	for kmer_start_pos in range (0, len(sequence)-kmer_length+1): 
+		kmer_end_pos = kmer_start_pos + kmer_length
+		kmer_seq = sequence[kmer_start_pos:kmer_end_pos]
+		kmers_list.append(kmer_seq) 
+	return kmers_list
+
+#print(sequence_to_kmer_list('AATTCCGG', 4)) 
+
+def main(): 
+	progname = sys.argv[0]
+	usage = "\n\n\tusage: {} sequence kmer_length\n\n\n".format(progname) 
+
+	if len(sys.argv) <3:
+		sys.stderr.write(usage) 
+		sys.exit(1) 
+
+	#capture command line arguments
+	sequence = sys.argv[1]
+	kmer_length = int(sys.argv[2]) 
+	
+	kmers = sequence_to_kmer_list(sequence, kmer_length)
+	
+	print (kmers) 
+	sys.exit (0) # always good practice to indicate worked ok
+
+
+if __name__ == '__main__':
+	main()  
